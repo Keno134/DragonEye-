8301ca03e56efe2d2be057a9e45c6b34067b0eaf
Keno134 <109069819+Keno134@users.noreply.github.com>
2022-10-28 16:31:24 +0530

    Update __main__.py

--- Exon/__main__.py
+++ Exon/__main__.py
@@ -300,8 +300,8 @@ def start(update: Update, context: CallbackContext):
                 disable_web_page_preview=False,
             )
     else:
-        update.effective_message.reply_video(                            
-            START_VIDEO,
+        update.effective_message.reply_img(                            
+            START_IMG,
             caption="ʜᴇʏ `{}`,\n\nɪ ᴀᴍ ᴀʟɪᴠᴇ Bɪᴛᴄʜ !\n➥ᴜᴘᴛɪᴍᴇ: `{}` \n➥ᴜsᴇʀs: `{}` \n➥ᴄʜᴀᴛs: `{}` ".format(
                 usr.first_name,
                 uptime,
