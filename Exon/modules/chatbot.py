ed9ae1d4d2a489ce3df4bcfe60859a8a12f0323d
ARAN アラン <115202562+TaktAsahina@users.noreply.github.com>
2022-10-31 01:00:50 +0530

    Update chatbot.py

--- Exon/modules/chatbot.py
+++ Exon/modules/chatbot.py
@@ -142,7 +142,7 @@ def chatbot(update: Update, context: CallbackContext):
         Message = message.text
         bot.send_chat_action(chat_id, action="typing")
         kukiurl = requests.get(
-            f"https://api.bakufu.tech/api/chatbot/cleverbot?name=DragonEye&owner=DRAGONEYE&message={anon}"
+            f"https://api.bakufu.tech/api/chatbot/cleverbot?name=DragonEye&owner=DRAGONEYE&message={Message}"
         )
 
         Kuki = json.loads(kukiurl.text)
