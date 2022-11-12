b8d8a1ae221f6735c5117c00bdf49f98ee59bf27
Keno134 <109069819+Keno134@users.noreply.github.com>
2022-11-13 01:38:05 +0530

    Update chatbot.py

--- Exon/modules/chatbot.py
+++ Exon/modules/chatbot.py
@@ -142,7 +142,7 @@ def chatbot(update: Update, context: CallbackContext):
         Message = message.text
         bot.send_chat_action(chat_id, action="typing")
         kukiurl = requests.get(
-            f"http://api.roseloverx.com/api/chatbot?message={anon}"
+            f"https://api.bakufu.tech/api/chatbot/cleverbot?name=DragonEye&owner=DRAGONEYE&message="
         )
 
         Kuki = json.loads(kukiurl.text)
